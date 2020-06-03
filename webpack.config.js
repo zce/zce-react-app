2b015b18839ea9d85b6706a2c74028446126beb6
zce <w@zce.me>
2020-06-03 14:06:02 +0800

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/, 
+        loader: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+}
